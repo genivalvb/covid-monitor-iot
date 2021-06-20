d8e39fe0ee1af0ede5e29ce4b3092a7ac85eacd1
Genival Silva <genivalvb@yahoo.com.br>
2021-06-20 11:03:42 -0300

    Primeiro commit

---
+++ src/main/java/com/covidmonitoriot/covidmonitoriot/CovidMonitorIotApplication.java
@@ -0,0 +1,13 @@
+package com.covidmonitoriot.covidmonitoriot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidMonitorIotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidMonitorIotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/covidmonitoriot/covidmonitoriot/CovidMonitorIotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covidmonitoriot.covidmonitoriot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidMonitorIotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
